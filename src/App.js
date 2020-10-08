018f2463007c89bf7312b4d90ca4bb68bf0c2c6f
NinaaMaric <nina.maric@amplitudo.me>
2020-10-08 12:17:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
+      console.log("We are in");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+      console.log("details do not match");
+      setError("Details do not match")
+    }
+  };
+
+  const Logout = () => {
+    console.log("logout");
+    setUser({name: "", email: "" })
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nina
-        </p>
-      </header>
+      {user.email != "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span> {user.name} </span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
